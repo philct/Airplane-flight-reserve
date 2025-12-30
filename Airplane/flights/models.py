6369e694a4791a735b3b2f0556ff95d7e6ec7498
philct <philct00@gmail.com>
2025-12-29 22:17:18 -0500

    Create the model Airport and Flight

--- Airplane/flights/models.py
+++ Airplane/flights/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    city = models.CharField(max_length=20)
+    code = models.CharField(max_length=3)
+
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departures')
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrivals')
+    length = models.IntegerField(max_length=50)
+
+
+
+
+
+
--- Airplane/flights/views.py
+++ Airplane/flights/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import request
+from .models import Airport, Flight
 
 # Create your views here.
 def index(request):
-    return render(request, 'flights/index.html')
+    return render(request, 'flights/index.html', {
+        'flights': Flight.objects.all()
+    })
