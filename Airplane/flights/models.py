c7c3bdbc5ce679cd289a81f8477192dae34ab044
philct <philct00@gmail.com>
2025-12-29 23:05:22 -0500

    Create details page, connect home page with it

--- Airplane/flights/models.py
+++ Airplane/flights/models.py
@@ -4,13 +4,18 @@ from django.db import models
 class Airport(models.Model):
     city = models.CharField(max_length=20)
     code = models.CharField(max_length=3)
+    
+    def __str__(self):
+        return f'{self.city} | {self.code}'
+    
 
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departures')
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrivals')
     length = models.IntegerField(max_length=50)
 
-
+    def __str__(self):
+        return f' From {self.origin} to {self.destination}. {self.length}hrs.'
 
 
 
--- Airplane/flights/views.py
+++ Airplane/flights/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request, 'flights/index.html', {
         'flights': Flight.objects.all()
     })
+
+def details(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, 'flights/details.html', {
+        'flight': flight,
+    })
